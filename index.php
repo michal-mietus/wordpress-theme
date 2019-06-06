b1d73e69f57009e67aee94d8b0c4737aa13d3d99
goodude123 <assasinmcpl@gmail.com>
2019-06-06 16:32:35 +0200

    Posts.

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 <?php 
 if ( have_posts() ) {
-	while ( have_posts() ) { ?>
-  <h2><?php echo the_post();?><h2>
-  
+	while ( have_posts() ) { 
+    the_post();?>
+
+    <h2><?php the_title(); ?><h2>
+    <p><?php the_content(); ?></p>
+    
   <?php
 	}
 }
