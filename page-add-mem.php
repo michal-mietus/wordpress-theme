08a221ccea0dda2848961c080e7736a217ad64c5
goodude123 <assasinmcpl@gmail.com>
2019-06-07 17:30:24 +0200

    Modified form.

--- page-add-mem.php
+++ page-add-mem.php
@@ -25,12 +25,20 @@
 <div id="respond">
   <?php echo $response; ?>
   <form action="<?php the_permalink(); ?>" method="post">
-    <p><label for="name">Name: <span>*</span> <br><input type="text" name="message_name" value="<?php echo esc_attr($_POST['message_name']); ?>"></label></p>
-    <p><label for="message_email">Email: <span>*</span> <br><input type="text" name="message_email" value="<?php echo esc_attr($_POST['message_email']); ?>"></label></p>
-    <p><label for="message_text">Message: <span>*</span> <br><textarea type="text" name="message_text"><?php echo esc_textarea($_POST['message_text']); ?></textarea></label></p>
-    <p><label for="message_human">Human Verification: <span>*</span> <br><input type="text" style="width: 60px;" name="message_human"> + 3 = 5</label></p>
-    <input type="hidden" name="submitted" value="1">
-    <p><input type="submit"></p>
+    <label for="title">Title <span>*</span> 
+      <input type="text" name="title" value="<?php echo esc_attr($_POST['description']); ?>">
+    </label>
+    <label for="description">Description:
+      <textarea type="text" name="description"><?php echo esc_textarea($_POST['description']); ?></textarea>
+    </label>
+    <label for="image">
+      <input type="file" name="image" accept="image/*">
+    </label>
+    <label for="video">
+      <input type="file" name="video" accept="video/*">
+    </label>
+    
+    <input type="submit">
   </form>
 </div>
 
