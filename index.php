c95cbd2325f5e66654ad306c8f914cd08fd9d096
goodude123 <assasinmcpl@gmail.com>
2019-06-07 18:09:22 +0200

    Link to single post.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if ( have_posts() ) {
 	while ( have_posts() ) { 
     the_post();?>
 
-    <h2><?php the_title(); ?></h2>
+    <a href="<?php the_permalink() ?>"><h2><?php the_title(); ?></h2></a>
     <p><?php the_content(); ?></p>
     <hr>
 
